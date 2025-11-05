3d5b73731187d99380439261ecbc0f0ae404118a
nothingx-x <sirmrmmr@gmail.com>
2025-11-05 19:33:28 +0330

    added test image loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>MyBrawl</div>;
+  return (
+    <div>
+      MyBrawl
+      <Image
+        src="https://ugvxlsnnuutbpy2w.public.blob.vercel-storage.com/photo_2025-10-25_10-33-24.png"
+        width={200}
+        height={200}
+        alt="mongo"
+      />
+    </div>
+  );
 }
